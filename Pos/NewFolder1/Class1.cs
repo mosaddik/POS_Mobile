876c59ecc2cae8f1cbcb2444b5a3c15c77a2802a
mosaddik <mosaddikbinhafiz@gmail.com>
2017-04-10 19:45:20 +0600

    Class1 Modified

--- Pos/NewFolder1/Class1.cs
+++ Pos/NewFolder1/Class1.cs
@@ -7,5 +7,8 @@ namespace Pos.NewFolder1
 {
     public class Class1
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Mobile { get; set; }
     }
 }
