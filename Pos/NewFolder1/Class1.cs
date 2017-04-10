74ff32f3829356721d9ef85cc9d86f8b6424a719
mosaddik <mosaddikbinhafiz@gmail.com>
2017-04-10 20:43:03 +0600

    add

--- Pos/NewFolder1/Class1.cs
+++ Pos/NewFolder1/Class1.cs
@@ -10,5 +10,6 @@ namespace Pos.NewFolder1
         public int Id { get; set; }
         public string Name { get; set; }
         public string Mobile { get; set; }
+        public string AD { get; set; }
     }
 }
